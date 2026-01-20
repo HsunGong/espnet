70f62f53c2aaa8c1eb5dd76af3a2af2fc1d5be0a
Xun Gong <gongxuncd@gmail.com>
2026-01-20 09:15:34 +0800

    Some modify on datasets

--- egs2/librimix/enh1/local/data.sh
+++ egs2/librimix/enh1/local/data.sh
@@ -61,7 +61,7 @@ fi
 cdir=$PWD
 
 
-git clone https://github.com/JorisCos/LibriMix ./data/LibriMix
+[ -d ./data/LibriMix ] || git clone https://github.com/JorisCos/LibriMix ./data/LibriMix
 
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     log "stage 0: Downloading WHAM! noise data to '${cdir}/data/wham_noise'"
@@ -70,10 +70,10 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     if [ -z "${wham_noise}" ]; then
         # 17.65 GB unzipping to 35 GB
         mkdir -p ${cdir}/data/wham_noise
-        wham_noise_url=https://storage.googleapis.com/whisper-public/wham_noise.zip
+        wham_noise_url=https://my-bucket-a8b4b49c25c811ee9a7e8bba05fa24c7.s3.amazonaws.com/wham_noise.zip # https://storage.googleapis.com/whisper-public/wham_noise.zip
         wget --continue -O "${cdir}/data/wham_noise.zip" ${wham_noise_url}
         num_wavs=$(find "${cdir}/data/wham_noise" -iname "*.wav" | wc -l)
-        if [ "${num_wavs}" = "4" ]; then
+        if [ "${num_wavs}" -ge "4" ]; then
             echo "'${cdir}/data/wham_noise/' already exists. Skipping..."
         else
             unzip "${cdir}/data/wham_noise.zip" -d "${cdir}/data/"
@@ -86,13 +86,15 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     fi
 fi
 
+if [ -z "${wham_noise}" ]; then
+    wham_noise="${cdir}/data/wham_noise"
+fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     log "stage 1: Data simulation"
     (
     cd ./data/LibriMix
     librimix_outdir=./libri_mix
-
 
     python scripts/augment_train_noise.py --wham_dir ${cdir}/data/wham_noise
     # shellcheck disable=SC2043
@@ -105,53 +107,74 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
         --freqs $sample_rate \
         --modes $min_or_max \
         --types mix_clean mix_both mix_single
-
     )
 fi
 
-
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
-    log "stage 2: LibriMix data preparation"
+    log "stage 2: LibriMix data preparation (mix_both)"
 
     librimix="data/LibriMix/libri_mix/Libri2Mix"
     for dset in dev test train; do
-        mkdir -p "data/${dset}"
+        mkdir -p "data_mix_both/${dset}"
         if [ "$dset" = "train" ]; then
-            cat ${librimix}/wav${sample_rate}/${min_or_max}/metadata/mixture_train-*_mix_both.csv | grep -v mixture_ID | sort -u > "data/${dset}/tmp"
+            cat ${librimix}/wav${sample_rate}/${min_or_max}/metadata/mixture_train-*_mix_both.csv | grep -v mixture_ID | sort -u > "data_mix_both/${dset}/tmp"
         else
-            grep -v mixture_ID ${librimix}/wav${sample_rate}/${min_or_max}/metadata/mixture_${dset}_mix_both.csv | sort -u > "data/${dset}/tmp"
+            grep -v mixture_ID ${librimix}/wav${sample_rate}/${min_or_max}/metadata/mixture_${dset}_mix_both.csv | sort -u > "data_mix_both/${dset}/tmp"
         fi
-        awk -F ',' '{print $1, $1}' "data/${dset}/tmp" > "data/${dset}/utt2spk"
-        awk -F ',' '{print $1, $1}' "data/${dset}/tmp" > "data/${dset}/spk2utt"
-        awk -F ',' '{print $1, $2}' "data/${dset}/tmp" > "data/${dset}/wav.scp"
-        awk -F ',' '{print $1, $3}' "data/${dset}/tmp" > "data/${dset}/spk1.scp"
-        awk -F ',' '{print $1, $4}' "data/${dset}/tmp" > "data/${dset}/spk2.scp"
+        awk -F ',' '{print $1, $1}' "data_mix_both/${dset}/tmp" > "data_mix_both/${dset}/utt2spk"
+        awk -F ',' '{print $1, $1}' "data_mix_both/${dset}/tmp" > "data_mix_both/${dset}/spk2utt"
+        awk -F ',' '{print $1, $2}' "data_mix_both/${dset}/tmp" > "data_mix_both/${dset}/wav.scp"
+        awk -F ',' '{print $1, $3}' "data_mix_both/${dset}/tmp" > "data_mix_both/${dset}/spk1.scp"
+        awk -F ',' '{print $1, $4}' "data_mix_both/${dset}/tmp" > "data_mix_both/${dset}/spk2.scp"
         if [ $num_spk -eq 2 ]; then
-            awk -F ',' '{print $1, $5}' "data/${dset}/tmp" > "data/${dset}/noise1.scp"
+            awk -F ',' '{print $1, $5}' "data_mix_both/${dset}/tmp" > "data_mix_both/${dset}/noise1.scp"
         else
-            awk -F ',' '{print $1, $5}' "data/${dset}/tmp" > "data/${dset}/spk3.scp"
-            awk -F ',' '{print $1, $6}' "data/${dset}/tmp" > "data/${dset}/noise1.scp"
+            awk -F ',' '{print $1, $5}' "data_mix_both/${dset}/tmp" > "data_mix_both/${dset}/spk3.scp"
+            awk -F ',' '{print $1, $6}' "data_mix_both/${dset}/tmp" > "data_mix_both/${dset}/noise1.scp"
         fi
-        rm "data/${dset}/tmp"
+        rm "data_mix_both/${dset}/tmp"
     done
 fi
-
 
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
-    log "stage 3: Prepare data files for train-100 and train-360"
-    mkdir -p data/{train-100,train-360}
+    log "stage 3: LibriMix data preparation (mix_single)"
 
-    for subset in "train-100" "train-360"; do
-        grep -e "${subset}" "data/train/wav.scp" > "data/${subset}/wav.scp"
-        for f in data/train/*.scp; do
-            [ "$f" = "data/train/wav.scp" ] || utils/filter_scp.pl "data/${subset}/wav.scp" "$f" > "data/${subset}/$(basename $f)"
-        done
-        utils/filter_scp.pl "data/${subset}/wav.scp" data/train/utt2spk > data/${subset}/utt2spk
+    librimix="data/LibriMix/libri_mix/Libri2Mix"
+    for dset in dev test train; do
+        mkdir -p "data_mix_single/${dset}"
+        if [ "$dset" = "train" ]; then
+            cat ${librimix}/wav${sample_rate}/${min_or_max}/metadata/mixture_train-*_mix_single.csv | grep -v mixture_ID | sort -u > "data_mix_single/${dset}/tmp"
+        else
+            grep -v mixture_ID ${librimix}/wav${sample_rate}/${min_or_max}/metadata/mixture_${dset}_mix_single.csv | sort -u > "data_mix_single/${dset}/tmp"
+        fi
+        awk -F ',' '{print $1, $1}' "data_mix_single/${dset}/tmp" > "data_mix_single/${dset}/utt2spk"
+        awk -F ',' '{print $1, $1}' "data_mix_single/${dset}/tmp" > "data_mix_single/${dset}/spk2utt"
+        awk -F ',' '{print $1, $2}' "data_mix_single/${dset}/tmp" > "data_mix_single/${dset}/wav.scp"
+        awk -F ',' '{print $1, $3}' "data_mix_single/${dset}/tmp" > "data_mix_single/${dset}/spk1.scp"
+        awk -F ',' '{print $1, $4}' "data_mix_single/${dset}/tmp" > "data_mix_single/${dset}/noise1.scp"
+        rm "data_mix_single/${dset}/tmp"
     done
 fi
 
+
 if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
-    log "stage 4: LibriMix Transcripts Files Prepration."
+    log "stage 4: Prepare data files for train-100 and train-360"
+
+    for mix_type in mix_both mix_single; do
+        mkdir -p data_${mix_type}/{train-100,train-360}
+
+        for subset in "train-100" "train-360"; do
+            grep -e "${subset}" "data_${mix_type}/train/wav.scp" > "data_${mix_type}/${subset}/wav.scp"
+            for f in data_${mix_type}/train/*.scp; do
+                [ "$f" = "data_${mix_type}/train/wav.scp" ] || utils/filter_scp.pl "data_${mix_type}/${subset}/wav.scp" "$f" > "data_${mix_type}/${subset}/$(basename $f)"
+            done
+            utils/filter_scp.pl "data_${mix_type}/${subset}/wav.scp" data_${mix_type}/train/utt2spk > data_${mix_type}/${subset}/utt2spk
+        done
+    done
+fi
+
+if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
+    log "stage 5: LibriMix Transcripts Files Prepration."
 
     mkdir -p data/local
     for dset in "train-clean-100" "train-clean-360" "dev-clean" "test-clean"; do
@@ -160,17 +183,25 @@ if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
     done
     cat data/local/train-clean-100.txt data/local/train-clean-360.txt > data/local/train_clean_460.txt
 
-    for dset in "dev" "test" "train" "train-100" "train-360"; do
-        if [[ " ${dset} " =~ train* ]]; then
-            src_ls_dset_txt="train_clean_460.txt"
-        else
-            src_ls_dset_txt="${dset}-clean.txt"
-        fi
+    for mix_type in mix_both mix_single; do
+        for dset in "dev" "test" "train" "train-100" "train-360"; do
+            if [[ " ${dset} " =~ train* ]]; then
+                src_ls_dset_txt="train_clean_460.txt"
+            else
+                src_ls_dset_txt="${dset}-clean.txt"
+            fi
 
-        for i in $(seq ${num_spk}); do
-            awk -v idx=${i} '(FILENAME==ARGV[1]) {text[$1]=$0} (FILENAME==ARGV[2]) {split($1, lst, "_"); uttname=lst[idx]; print($1, text[uttname])}' \
-                data/local/${src_ls_dset_txt} data/${dset}/wav.scp | \
-                cut -d" " -f1,3- > data/${dset}/text_spk${i}
+            if [ "${mix_type}" = "mix_single" ]; then
+                nspk=1
+            else
+                nspk=${num_spk}
+            fi
+
+            for i in $(seq ${nspk}); do
+                awk -v idx=${i} '(FILENAME==ARGV[1]) {text[$1]=$0} (FILENAME==ARGV[2]) {split($1, lst, "_"); uttname=lst[idx]; print($1, text[uttname])}' \
+                    data/local/${src_ls_dset_txt} data_${mix_type}/${dset}/wav.scp | \
+                    cut -d" " -f1,3- > data_${mix_type}/${dset}/text_spk${i}
+            done
         done
     done
 
--- egs2/vctk/tts1/local/data_prep.sh
+++ egs2/vctk/tts1/local/data_prep.sh
@@ -43,92 +43,95 @@ for spk in ${spks}; do
     [ ! -e "${db}/lab/mono/${spk}" ] && \
         echo "${spk} does not exist." >&2 && exit 1;
 
-    [ ! -e data/${spk}_train ] && mkdir -p data/${spk}_train
+    #[ ! -e data/${spk}_train ] && mkdir -p data/${spk}_train
 
-    # set filenames
-    scp=data/${spk}_train/wav.scp
-    utt2spk=data/${spk}_train/utt2spk
-    text=data/${spk}_train/text
-    segments=data/${spk}_train/segments
-    spk2utt=data/${spk}_train/spk2utt
+    ## set filenames
+    #scp=data/${spk}_train/wav.scp
+    #utt2spk=data/${spk}_train/utt2spk
+    #text=data/${spk}_train/text
+    #segments=data/${spk}_train/segments
+    #spk2utt=data/${spk}_train/spk2utt
 
-    # check file existence
-    [ -e "${scp}" ] && rm "${scp}"
-    [ -e "${utt2spk}" ] && rm "${utt2spk}"
-    [ -e "${text}" ] && rm "${text}"
-    [ -e "${segments}" ] && rm "${segments}"
+    ## check file existence
+    #[ -e "${scp}" ] && rm "${scp}"
+    #[ -e "${utt2spk}" ] && rm "${utt2spk}"
+    #[ -e "${text}" ] && rm "${text}"
+    #[ -e "${segments}" ] && rm "${segments}"
 
-    # make scp, text, and segments
-    find "${db}/wav48/${spk}" -follow -name "*.wav" | sort | while read -r wav; do
-        id=$(basename "${wav}" | sed -e "s/\.[^\.]*$//g")
-        lab=${db}/lab/mono/${spk}/${id}.lab
-        txt=${db}/txt/${spk}/${id}.txt
+    ## make scp, text, and segments
+    #find "${db}/wav48/${spk}" -follow -name "*.wav" | sort | while read -r wav; do
+    #    id=$(basename "${wav}" | sed -e "s/\.[^\.]*$//g")
+    #    lab=${db}/lab/mono/${spk}/${id}.lab
+    #    txt=${db}/txt/${spk}/${id}.txt
 
-        # check lab existence
-        if [ ! -e "${lab}" ]; then
-            echo "${id} does not have a label file. skipped."
-            continue
-        fi
-        if [ ! -e "${txt}" ]; then
-            echo "${id} does not have a text file. skipped."
-            continue
-        fi
+    #    # check lab existence
+    #    if [ ! -e "${lab}" ]; then
+    #        echo "${id} does not have a label file. skipped."
+    #        continue
+    #    fi
+    #    if [ ! -e "${txt}" ]; then
+    #        echo "${id} does not have a text file. skipped."
+    #        continue
+    #    fi
 
-        echo "${id} ${wav}" >> "${scp}"
-        echo "${id} ${spk}" >> "${utt2spk}"
-        echo "${id} $(cat ${txt})" >> "${text}"
+    #    echo "${id} ${wav}" >> "${scp}"
+    #    echo "${id} ${spk}" >> "${utt2spk}"
+    #    echo "${id} $(cat ${txt})" >> "${text}"
 
-        utils/utt2spk_to_spk2utt.pl "${utt2spk}" > "${spk2utt}"
+    #    utils/utt2spk_to_spk2utt.pl "${utt2spk}" > "${spk2utt}"
 
-        # parse start and end time from HTS-style mono label
-        idx=1
-        while true; do
-            next_idx=$((idx+1))
-            next_symbol=$(sed -n "${next_idx}p" "${lab}" | awk '{print $3}')
-            if [ "${next_symbol}" != "pau" ]; then
-                start_nsec=$(sed -n "${idx}p" "${lab}" | awk '{print $2}')
-                break
-            fi
-            idx=${next_idx}
-        done
-        idx=$(wc -l < "${lab}")
-        while true; do
-            prev_idx=$((idx-1))
-            prev_symbol=$(sed -n "${prev_idx}p" "${lab}" | awk '{print $3}')
-            if [ "${prev_symbol}" != "pau" ]; then
-                end_nsec=$(sed -n "${idx}p" "${lab}" | awk '{print $1}')
-                break
-            fi
-            idx=${prev_idx}
-        done
-        start_sec=$(echo "${start_nsec}*0.0000001" | bc | sed "s/^\./0./")
-        end_sec=$(echo "${end_nsec}*0.0000001" | bc | sed "s/^\./0./")
-        echo "${id} ${id} ${start_sec} ${end_sec}" >> "${segments}"
-    done
+    #    # parse start and end time from HTS-style mono label
+    #    idx=1
+    #    while true; do
+    #        next_idx=$((idx+1))
+    #        next_symbol=$(sed -n "${next_idx}p" "${lab}" | awk '{print $3}')
+    #        if [ "${next_symbol}" != "pau" ]; then
+    #            start_nsec=$(sed -n "${idx}p" "${lab}" | awk '{print $2}')
+    #            break
+    #        fi
+    #        idx=${next_idx}
+    #    done
+    #    idx=$(wc -l < "${lab}")
+    #    while true; do
+    #        prev_idx=$((idx-1))
+    #        prev_symbol=$(sed -n "${prev_idx}p" "${lab}" | awk '{print $3}')
+    #        if [ "${prev_symbol}" != "pau" ]; then
+    #            end_nsec=$(sed -n "${idx}p" "${lab}" | awk '{print $1}')
+    #            break
+    #        fi
+    #        idx=${prev_idx}
+    #    done
+    #    start_sec=$(echo "${start_nsec}*0.0000001" | bc | sed "s/^\./0./")
+    #    end_sec=$(echo "${end_nsec}*0.0000001" | bc | sed "s/^\./0./")
+    #    echo "${id} ${id} ${start_sec} ${end_sec}" >> "${segments}"
+    #done
 
-    # split
-    num_all=$(wc -l < "${scp}")
-    num_deveval=$((num_dev + num_eval))
-    num_train=$((num_all - num_deveval))
-    utils/subset_data_dir.sh --last "data/${spk}_train" "${num_deveval}" "data/${spk}_deveval"
-    utils/subset_data_dir.sh --first "data/${spk}_deveval" "${num_dev}" "data/${spk}_${eval_set}"
-    utils/subset_data_dir.sh --last "data/${spk}_deveval" "${num_eval}" "data/${spk}_${dev_set}"
-    utils/subset_data_dir.sh --first "data/${spk}_train" "${num_train}" "data/${spk}_${train_set}"
+    ## split
+    #num_all=$(wc -l < "${scp}")
+    #num_deveval=$((num_dev + num_eval))
+    #num_train=$((num_all - num_deveval))
+    #utils/subset_data_dir.sh --last "data/${spk}_train" "${num_deveval}" "data/${spk}_deveval"
+    #utils/subset_data_dir.sh --first "data/${spk}_deveval" "${num_dev}" "data/${spk}_${eval_set}"
+    #utils/subset_data_dir.sh --last "data/${spk}_deveval" "${num_eval}" "data/${spk}_${dev_set}"
+    #utils/subset_data_dir.sh --first "data/${spk}_train" "${num_train}" "data/${spk}_${train_set}"
+    #echo "Prepared data for speaker: ${spk}"
 
-    # remove tmp directories
-    rm -rf "data/${spk}_train"
-    rm -rf "data/${spk}_deveval"
+    ## remove tmp directories
+    #rm -rf "data/${spk}_train"
+    #rm -rf "data/${spk}_deveval"
 
     train_data_dirs+=" data/${spk}_${train_set}"
     dev_data_dirs+=" data/${spk}_${dev_set}"
     eval_data_dirs+=" data/${spk}_${eval_set}"
 done
 
+wait
+
 utils/combine_data.sh data/${train_set} ${train_data_dirs}
 utils/combine_data.sh data/${dev_set} ${dev_data_dirs}
 utils/combine_data.sh data/${eval_set} ${eval_data_dirs}
 
 # remove tmp directories
-rm -rf data/p[0-9]*
+# rm -rf data/p[0-9]*
 
 echo "Successfully prepared data."
